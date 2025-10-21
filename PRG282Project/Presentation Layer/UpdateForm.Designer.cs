7d249a51cb7202ecee6db53c90d987361b2bc155
tumxa27 <tumelobapela97@gmail.com>
2025-10-21 21:54:09 +0200

    Add files via upload

--- PRG282Project/Presentation Layer/UpdateForm.Designer.cs
+++ PRG282Project/Presentation Layer/UpdateForm.Designer.cs
@@ -53,20 +53,21 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Recline Light Demo", 12F);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(64, 77);
+            this.label2.Location = new System.Drawing.Point(72, 96);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 28);
+            this.label2.Size = new System.Drawing.Size(95, 29);
             this.label2.TabIndex = 9;
             this.label2.Text = "Hero ID";
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(445, 66);
+            this.listView1.Location = new System.Drawing.Point(501, 82);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(399, 372);
+            this.listView1.Size = new System.Drawing.Size(448, 464);
             this.listView1.TabIndex = 10;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
@@ -75,9 +76,10 @@
             this.btnView.BackColor = System.Drawing.Color.White;
             this.btnView.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnView.ForeColor = System.Drawing.Color.SteelBlue;
-            this.btnView.Location = new System.Drawing.Point(565, 461);
+            this.btnView.Location = new System.Drawing.Point(636, 576);
+            this.btnView.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnView.Name = "btnView";
-            this.btnView.Size = new System.Drawing.Size(159, 42);
+            this.btnView.Size = new System.Drawing.Size(179, 52);
             this.btnView.TabIndex = 11;
             this.btnView.Text = "View Superheros";
             this.btnView.UseVisualStyleBackColor = false;
@@ -87,9 +89,10 @@
             this.btnBack.BackColor = System.Drawing.Color.White;
             this.btnBack.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBack.ForeColor = System.Drawing.Color.SteelBlue;
-            this.btnBack.Location = new System.Drawing.Point(26, 498);
+            this.btnBack.Location = new System.Drawing.Point(29, 622);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(82, 29);
+            this.btnBack.Size = new System.Drawing.Size(92, 36);
             this.btnBack.TabIndex = 12;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = false;
@@ -97,45 +100,48 @@
             // 
             // txtHeroID
             // 
-            this.txtHeroID.Location = new System.Drawing.Point(186, 77);
+            this.txtHeroID.Location = new System.Drawing.Point(209, 96);
+            this.txtHeroID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtHeroID.Name = "txtHeroID";
-            this.txtHeroID.Size = new System.Drawing.Size(227, 22);
+            this.txtHeroID.Size = new System.Drawing.Size(255, 26);
             this.txtHeroID.TabIndex = 15;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Recline Light Demo", 12F);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(64, 150);
+            this.label1.Location = new System.Drawing.Point(72, 188);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 28);
+            this.label1.Size = new System.Drawing.Size(124, 29);
             this.label1.TabIndex = 16;
             this.label1.Text = "Full Name";
             // 
             // txtFullName
             // 
-            this.txtFullName.Location = new System.Drawing.Point(186, 153);
+            this.txtFullName.Location = new System.Drawing.Point(209, 191);
+            this.txtFullName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtFullName.Name = "txtFullName";
-            this.txtFullName.Size = new System.Drawing.Size(227, 22);
+            this.txtFullName.Size = new System.Drawing.Size(255, 26);
             this.txtFullName.TabIndex = 17;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Recline Light Demo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(64, 220);
+            this.label3.Location = new System.Drawing.Point(72, 275);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 28);
+            this.label3.Size = new System.Drawing.Size(56, 29);
             this.label3.TabIndex = 18;
             this.label3.Text = "Age";
             // 
             // nupAge
             // 
-            this.nupAge.Location = new System.Drawing.Point(186, 226);
+            this.nupAge.Location = new System.Drawing.Point(209, 282);
+            this.nupAge.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.nupAge.Maximum = new decimal(new int[] {
             80,
             0,
@@ -147,7 +153,7 @@
             0,
             0});
             this.nupAge.Name = "nupAge";
-            this.nupAge.Size = new System.Drawing.Size(86, 22);
+            this.nupAge.Size = new System.Drawing.Size(97, 26);
             this.nupAge.TabIndex = 19;
             this.nupAge.Value = new decimal(new int[] {
             16,
@@ -159,11 +165,11 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Recline Light Demo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(65, 288);
+            this.label4.Location = new System.Drawing.Point(73, 360);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(115, 28);
+            this.label4.Size = new System.Drawing.Size(146, 29);
             this.label4.TabIndex = 20;
             this.label4.Text = "Superpower";
             // 
@@ -179,28 +185,30 @@
             "Shapeshifting",
             "Flight",
             "Time manipulation"});
-            this.cmbSuperpower.Location = new System.Drawing.Point(186, 291);
+            this.cmbSuperpower.Location = new System.Drawing.Point(209, 364);
+            this.cmbSuperpower.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmbSuperpower.Name = "cmbSuperpower";
-            this.cmbSuperpower.Size = new System.Drawing.Size(227, 24);
+            this.cmbSuperpower.Size = new System.Drawing.Size(255, 28);
             this.cmbSuperpower.TabIndex = 21;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Recline Light Demo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(67, 357);
+            this.label5.Location = new System.Drawing.Point(75, 446);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(113, 28);
+            this.label5.Size = new System.Drawing.Size(143, 29);
             this.label5.TabIndex = 22;
             this.label5.Text = "Exam Score";
             // 
             // nupExamScore
             // 
-            this.nupExamScore.Location = new System.Drawing.Point(186, 361);
+            this.nupExamScore.Location = new System.Drawing.Point(209, 451);
+            this.nupExamScore.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.nupExamScore.Name = "nupExamScore";
-            this.nupExamScore.Size = new System.Drawing.Size(86, 22);
+            this.nupExamScore.Size = new System.Drawing.Size(97, 26);
             this.nupExamScore.TabIndex = 23;
             // 
             // btnUpdate
@@ -208,31 +216,33 @@
             this.btnUpdate.BackColor = System.Drawing.Color.White;
             this.btnUpdate.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdate.ForeColor = System.Drawing.Color.SteelBlue;
-            this.btnUpdate.Location = new System.Drawing.Point(121, 422);
+            this.btnUpdate.Location = new System.Drawing.Point(136, 528);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(228, 48);
+            this.btnUpdate.Size = new System.Drawing.Size(256, 60);
             this.btnUpdate.TabIndex = 24;
             this.btnUpdate.Text = "Update Superhero";
             this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(15, 16);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(79, 60);
+            this.pictureBox1.Size = new System.Drawing.Size(89, 75);
             this.pictureBox1.TabIndex = 25;
             this.pictureBox1.TabStop = false;
             // 
             // UpdateForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(898, 552);
+            this.ClientSize = new System.Drawing.Size(1010, 690);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.nupExamScore);
@@ -249,6 +259,7 @@
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "UpdateForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "UpdateForm";
